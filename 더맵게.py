0165788b68a7bbee02104d86be9730479c104852
김정훈 <jhkim1380@naver.com>
2022-08-09 12:46:35 +0900

    코딩테스트

--- 더맵게.py
+++ 더맵게.py
@@ -1,27 +1,23 @@
+import heapq
 def solution(scoville, K):
     answer = 0
     #섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 
     # + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-    scoville.sort()
+    heap=[]
+    for i in scoville:
+        heapq.heappush(heap,i)
     cnt=0
-    if scoville[0]>=k:
-        return -1
-    elif scoville[0]<K:            
-        while scoville[0]<K:
-            cnt+=1
-            lst=[]
-            new_one=0
-            lst.append(scoville.pop(0))
-            lst.append(scoville.pop(0))
-            print(lst)
-            new_one=lst[0]+lst[1]*lst[1]
-            scoville.append(new_one)
-            scoville.sort()
+    while heap[0]<k:
+        try:
+            heapq.heappush(heap,heapq.heappop(heap)+(heapq.heappop(heap)*2))
+        except IndexError:
+            return -1
+        cnt+=1
 
     return cnt
 ##########################################
 #시간복잡도 충족X
 
-lst=[6,7,10,13]
+lst=[1,2,3,9,10,12]
 k=7
 print(solution(lst,k))
