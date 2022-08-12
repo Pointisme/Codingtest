ec1ebf0da38863601564a7c04a2a88586e25c86d
김정훈 <jhkim1380@naver.com>
2022-08-12 15:50:57 +0900

    coding test

---
+++ 이상한문자만들기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    new=[]
+    answer=''
+    new.append(s.split(" "))
+    for i in range(len(answer[0])):
+        for j in range(len(answer[0][i])):
+            
+
+print(solution("try hello world"))
